bf7fade061239dc731f828df69a7afa7d4605c78
KienKhang <trankienkhang2210@gmail.com>
2023-04-09 18:01:39 +0700

    feat: add index page

---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+div
+  | Hello, i am index Page
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
+
+<route lang="yaml">
+meta:
+  layout: default
+</route>
