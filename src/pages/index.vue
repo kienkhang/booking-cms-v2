f75cec0fe866911c6927dc9af28b108cde5f69bd
KienKhang <trankienkhang2210@gmail.com>
2023-04-09 18:03:43 +0700

    feat: goto test config

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,15 @@
 <template lang="pug">
 div
   | Hello, i am index Page
+  button(type="submit" class='p-2 rounded-md bg-green-500 text-white' @click='n()') Goto Test
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const router = useRouter()
+const n = () => {
+  router.push('/test')
+}
+</script>
 
 <style scoped></style>
 
