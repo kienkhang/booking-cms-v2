821284c161ba3972cbffa44bebb8a7320281fa05
KienKhang <trankienkhang2210@gmail.com>
2023-04-15 17:29:16 +0700

    feat: new component

---
+++ src/components/shared/EditorQuill.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+QuillEditor(theme='snow' toolbar='essential' content-type="html" style='height: 150px;' v-model:content='content')
+
+span {{ content }}
+</template>
+
+<script setup lang="ts">
+import '@vueup/vue-quill/dist/vue-quill.snow.css'
+import { QuillEditor } from '@vueup/vue-quill'
+import {} from '@vueup/vue-quill'
+
+const content = ref('')
+</script>
+
+<style scoped></style>
