6c8572ed7a1084fe6726a50ce9af9dc18345cd64
KienKhang <trankienkhang2210@gmail.com>
2023-04-25 10:55:57 +0700

    feat: update hotel form

--- src/pages/hotel/config.vue
+++ src/pages/hotel/config.vue
@@ -6,20 +6,19 @@ n-tabs.config-tab(
     :bar-width="500"
     justify-content='space-evenly'
     :tab-style='tabStyle'
-    style='max-width:700px'
     class="mx-auto"
     animated
   )
   //- tab label của Tab, name là value của Tab
-  n-tab-pane(name="basic" tab="Basic")
+  n-tab-pane(name="basic" tab="Basic" display-directive="show:lazy")
     hotel-config-form
-  n-tab-pane(name="additional" tab="Additional")
-    hotel-additional-form
+  n-tab-pane(name="photos" tab="Photos")
+    hotel-upload-form
 </template>
 
 <script setup lang="ts">
 import HotelConfigForm from '@/components/hotel/HotelConfigForm.vue'
-import HotelAdditionalForm from '@/components/hotel/HotelAdditionalForm.vue'
+import HotelUploadForm from '@/components/hotel/HotelUploadForm.vue'
 import { NTabs, NTabPane } from 'naive-ui'
 import { type CSSProperties } from 'vue'
 
