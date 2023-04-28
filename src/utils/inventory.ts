3d865ae60093072b2e09e27c7f4b171fd837629a
KienKhang <trankienkhang2210@gmail.com>
2023-04-28 13:21:00 +0700

    feat: select special date of week in month

--- src/utils/inventory.ts
+++ src/utils/inventory.ts
@@ -48,26 +48,26 @@ const ratepackages: IRatePackageCell[] = [
   }
 ]
 
-// filter list theo năm tháng
-
-// Hàm truyền vào bao gồm (year, month, roomnight)
+// -------------------------- FILTER AND RENDER LIST ------------------------
+// Params to render function (year, month, roomnight)
 const renderRoomNights = (
   realYear: number,
   realMonth: number,
   roomNight: IRoomNightCell[]
 ): IRoomNightCell[] => {
-  // Tính số ngày trong tháng
+  // Calculate days in month
   const dayOfMonth = dayjs().year(realYear).month(realMonth).daysInMonth()
-  // Lấy ngày đầu tiên của tháng đó, năm đó
+  // Get first date follow month and year
   const firstDate = dayjs().year(realYear).month(realMonth).date(1)
-  // Lấy ngày cuối cùng của tháng đó, năm đó
+  // Get last date follow month and year
   const lastDate = dayjs().year(realYear).month(realMonth).date(dayOfMonth)
 
-  // Filter lại list roomnight có ngày giữa đầu tháng và cuối tháng
+  // Filter list roomnight is between first and last month
   const filteredRoomNight = roomNight.filter((rn) =>
     dayjs(rn.availability_at).isBetween(firstDate, lastDate, 'year', '[]')
   )
-  // Tạo ra một mảng trống, sau đó loop từ đầu đến cuối tháng để render ra mảng dữ liệu tạm thời
+  // Create empty array, loop from first -> last month then fill cell
+  // Each cell contain -> date(YYY-MM-DD), id and quanity
   const renderSource = []
   for (let i = 1; i <= dayOfMonth; i++) {
     const formatted = dayjs().year(realYear).month(realMonth).date(i).format('YYYY-MM-DD')
@@ -89,24 +89,25 @@ const renderRoomNights = (
   })
   return calculateSource
 }
-// Hàm truyền vào bao gồm (year, month, ratepackage)
+// Params to render function (year, month, ratepackage)
 const renderRatePakages = (
   realYear: number,
   realMonth: number,
   ratePackage: IRatePackageCell[]
 ): IRatePackageCell[] => {
-  // Tính số ngày trong tháng
+  // Calculate days in month
   const dayOfMonth = dayjs().year(realYear).month(realMonth).daysInMonth()
-  // Lấy ngày đầu tiên của tháng đó, năm đó
+  // Get first date follow month and year
   const firstDate = dayjs().year(realYear).month(realMonth).date(1)
-  // Lấy ngày cuối cùng của tháng đó, năm đó
+  // Get last date follow month and year
   const lastDate = dayjs().year(realYear).month(realMonth).date(dayOfMonth)
 
-  // Filter lại list roomnight có ngày giữa đầu tháng và cuối tháng
+  // Filter list roomnight is between first and last month
   const filteredRoomNight = ratePackage.filter((rn) =>
     dayjs(rn.availability_at).isBetween(firstDate, lastDate, 'year', '[]')
   )
-  // Tạo ra một mảng trống, sau đó loop từ đầu đến cuối tháng để render ra mảng dữ liệu tạm thời
+  // Create empty array, loop from first -> last month then fill cell
+  // Each cell contain -> date(YYY-MM-DD), id and price
   const renderSource = []
   for (let i = 1; i <= dayOfMonth; i++) {
     const formatted = dayjs().year(realYear).month(realMonth).date(i).format('YYYY-MM-DD')
@@ -116,25 +117,28 @@ const renderRatePakages = (
       price: 0
     }
   }
-  // Sau đó dựa vào room night đã trả về từ API, tính toán xem có ngày nào trùng không, nếu có thì thay thế, để có dữ liệu
-  const calculateSource = renderSource.map((cell) => {
+  // Then based-on room night data from api, fill data to cell if the date is the same
+  const calculatedSource = renderSource.map((cell) => {
+    // find cell matched data
     const rn = filteredRoomNight.find((r) => r.availability_at === cell.availability_at)
-    // Nếu rn tồn tại, tức là có ngày đó trong mảng thì thay cả phần tử đó lun
+    // if rn exist -> assign data to this cell
     if (!!rn) {
       return rn
     }
-    // Không thì return cell luôn
+    // else return cell
     return cell
   })
-  return calculateSource
+  // return array after calculate
+  return calculatedSource
 }
 
 // render calendar
 const renderCalendar = (realYear: number, realMonth: number) => {
-  // Tính số ngày trong tháng
+  // Calculate days in month
   const dayOfMonth = dayjs().year(realYear).month(realMonth).daysInMonth()
 
-  // Tạo ra một mảng trống, sau đó loop từ đầu đến cuối tháng để render ra mảng dữ liệu tạm thời
+  // Create empty array, loop from first -> last month then fill cell
+  // Each cell contain -> date(YYY-MM-DD), short date(DD), day of week (ddd)
   const renderSource = []
   for (let i = 1; i <= dayOfMonth; i++) {
     const d = dayjs().year(realYear).month(realMonth).date(i)
@@ -152,4 +156,37 @@ const renderCalendar = (realYear: number, realMonth: number) => {
   return renderSource
 }
 
-export { renderCalendar, renderRatePakages, renderRoomNights, roomnights, ratepackages }
+// -------------------------- SELECT FOLLOW DAY OF WEEK ------------------------
+
+// Day of week is : [0 1 2 3 4 5 6]
+const selectMultipleDate = (dayOfWeek: number, month: number, year: number) => {
+  // DEFINE: firstSpecialDayOfTheMonth : f
+  if (dayOfWeek > 6 || dayOfWeek < 0) return
+  // Array contain special dayOfWeek in month
+  const specialDays: string[] = []
+  // Get month
+  // Get first special day of the month
+  let f = dayjs().year(year).month(month).startOf('month').day(dayOfWeek)
+  // if value first special of the month > 7, its not a first day, its a last day of the previous month
+  if (f.date() > 7) {
+    f = f.add(7, 'd')
+  }
+  // Compare f'month with set(month+year).month -> while add
+
+  while (f.month() === dayjs().year(year).month(month).month()) {
+    specialDays.push(f.format('YYYY-MM-DD'))
+    f = f.add(7, 'd')
+  }
+  return specialDays
+}
+// console.log('🐔🦢 ~ selectMultipleDate:', selectMultipleDate(2, 3, 2023))
+// selectMultipleDate(2, 3, 2021)
+
+export {
+  renderCalendar,
+  renderRatePakages,
+  renderRoomNights,
+  selectMultipleDate,
+  roomnights,
+  ratepackages
+}
