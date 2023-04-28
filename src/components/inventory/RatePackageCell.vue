5ce38b113625df9e0355e7dfcd3e8be2610185ac
KienKhang <trankienkhang2210@gmail.com>
2023-04-28 13:20:32 +0700

    feaT: conditional render cell

--- src/components/inventory/RatePackageCell.vue
+++ src/components/inventory/RatePackageCell.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.flex.justify-center.items-center.border.flex-shrink-0.select-none.cursor-pointer(class='w-20 h-8' :class="[{'bg-orange-500 text-white':notEmpty},{'bg-blue-500 text-white':selected}]"  :style='style' @click='doSelect')
+.flex.justify-center.items-center.border.flex-shrink-0.select-none.cursor-pointer(class='w-20 h-8' :class="[{'bg-blue-500 text-white':selected}, {'bg-orange-500 text-white':notEmpty && !selected}]"  :style='style' @click='doSelect')
   .font-medium {{ label }}
 </template>
 
@@ -10,24 +10,42 @@ import dayjs from 'dayjs'
 
 const props = defineProps<{
   data: IRatePackageCell
+  rateplan: string
 }>()
 
+// push message
+const message = useMessage()
+
 // useInventory
-const { selectedRatePackage } = storeToRefs(useInventory())
-const { exist, selectRatePackage } = useInventory()
+const { selectedRatePackage, adjustRate, mode, rateplan } = storeToRefs(useInventory())
+const { exist, selectRate, removeSelected } = useInventory()
 
 const doSelect = () => {
-  selectRatePackage({ date: props.data.availability_at, ratePlan: 'A' })
+  if (mode.value === 'dayOfWeek') {
+    message.error('Bạn không ở chế độ chọn tự do')
+    return
+  }
+  if (!exist(props.data.availability_at, selectedRatePackage.value)) {
+    selectRate({ date: props.data.availability_at, ratePlan: props.rateplan })
+    return
+  }
+  removeSelected(props.data.availability_at, selectedRatePackage)
 }
 
 // is selected cell
-const selected = computed(() => exist(props.data.availability_at, selectedRatePackage.value))
+const selected = computed(
+  () =>
+    exist(props.data.availability_at, selectedRatePackage.value) &&
+    rateplan.value === props.rateplan
+)
 // Label of content
 const label = computed(() => VND.format(props.data.price))
 // Check cell no value
 const notEmpty = computed(() => props.data.price !== 0)
 // Khóa khi ngày hiện tại lớn hơn ngày trong data
-const isDisabled = computed(() => dayjs().unix() >= dayjs(props.data.availability_at).unix())
+const isDisabled = computed(
+  () => dayjs().unix() >= dayjs(props.data.availability_at).unix() || !adjustRate.value
+)
 const style = computed(
   () => `${isDisabled.value ? 'background-color: #ccc; color: black; cursor: not-allowed;' : ''}`
 )
--- src/components/inventory/RoomNightCell.vue
+++ src/components/inventory/RoomNightCell.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.flex.justify-center.items-center.border.flex-shrink-0.select-none.cursor-pointer(class='w-20 h-8' :class="[{'bg-green-500 text-white':notEmpty},{'bg-blue-500 text-white':selected}]" :style='style' @click='doSelect')
+.flex.justify-center.items-center.border.flex-shrink-0.select-none.cursor-pointer(class='w-20 h-8' :class="[{'bg-green-500 text-white':notEmpty && !selected},{'bg-blue-500 text-white':selected}]" :style='style' @click='doSelect')
   .font-medium {{ label }}
 
 </template>
@@ -12,12 +12,24 @@ const props = defineProps<{
   data: IRoomNightCell
 }>()
 
+// push message
+const message = useMessage()
+
 // useInventory
-const { selectedRoomNight, room } = storeToRefs(useInventory())
-const { exist, selectRoomNight } = useInventory()
+const { selectedRoomNight, room, adjustRate, mode } = storeToRefs(useInventory())
+const { exist, selectRoomNight, removeSelected } = useInventory()
 
 const doSelect = () => {
-  selectRoomNight({ date: props.data.availability_at, roomType: room.value })
+  if (mode.value === 'dayOfWeek') {
+    message.error('Bạn không ở chế độ chọn tự do')
+    return
+  }
+
+  if (!exist(props.data.availability_at, selectedRoomNight.value)) {
+    selectRoomNight({ date: props.data.availability_at, roomType: room.value })
+    return
+  }
+  removeSelected(props.data.availability_at, selectedRoomNight)
 }
 
 // RENDER CONDITIONAL
@@ -28,7 +40,9 @@ const label = computed(() => props.data.quantity)
 // check no data
 const notEmpty = computed(() => props.data.quantity !== 0)
 // Khóa khi ngày hiện tại lớn hơn ngày trong data
-const isDisabled = computed(() => dayjs().unix() >= dayjs(props.data.availability_at).unix())
+const isDisabled = computed(
+  () => dayjs().unix() >= dayjs(props.data.availability_at).unix() || adjustRate.value
+)
 // Cell style
 const style = computed(
   () => `${isDisabled.value ? 'background-color: #ccc; color: black; cursor: not-allowed;' : ''}`
