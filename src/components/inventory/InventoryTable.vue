5603f0741ce457e1ca1825ffb109137f5666f34e
KienKhang <trankienkhang2210@gmail.com>
2023-04-28 13:20:16 +0700

    feat: update inventory table

--- src/components/inventory/InventoryTable.vue
+++ src/components/inventory/InventoryTable.vue
@@ -5,16 +5,16 @@ div.grid.grid-cols-12.text-center.select-none
     .border.border-r-0.border-b-0.h-10
     .border.border-r-0.font-semibold.py-2.text-xs.bg-pink-200 Số phòng 
     .border.border-r-0.font-medium.border-t-0.py-2(class='text-xs') Hoàn tiền
-    //- .border.border-r-0.font-medium.border-t-0.py-2(class='text-xs') Không hoàn tiền
+    .border.border-r-0.font-medium.border-t-0.py-2(class='text-xs') Không hoàn tiền
   .col-span-11.overflow-auto.border.scrollbar-gradient
     .flex.items-center.text-xs.font-bold
       DateCell(v-for='cell in calendar' :data='cell')
     .flex.items-center.font-bold(class='text-xs')
       RoomNightCell(v-for='cell in roomNightDatas' :data='cell')
     .flex.items-center.font-bold(class='text-xs')
-      RatePackageCell(v-for='cell in ratePackageDatas' :data='cell')
-    //- .flex.items-center.font-bold(class='text-xs')
-    //-   RatePackageCell(v-for='cell in ratePackageDatas' :data='cell')
+      RatePackageCell(v-for='cell in ratePackageDatas' :data='cell' rateplan='rateplan1')
+    .flex.items-center.font-bold(class='text-xs')
+      RatePackageCell(v-for='cell in ratePackageDatas' :data='cell' rateplan='rateplan2')
       
 
 
