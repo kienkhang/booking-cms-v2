258c7d3d1b84ae845b79bd5e163ff62be78a0895
KienKhang <trankienkhang2210@gmail.com>
2023-05-02 23:19:47 +0700

    feat: room tab & modal

---
+++ src/components/room/RoomTabs.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+n-tabs.config-tab(
+      type="line"
+      size='large'
+      default-value='basic'
+      :bar-width="500"
+      justify-content='space-evenly'
+      :tab-style='tabStyle'
+      class="mx-auto"
+      animated
+    )
+      //- tab label của Tab, name là value của Tab
+      n-tab-pane(name="basic" tab="Thông tin" display-directive="show:lazy")
+        room-form
+      n-tab-pane(name="photos" tab="Hình ảnh")
+        room-file-form
+
+</template>
+
+<script setup lang="ts">
+import RoomForm from './RoomForm.vue'
+import RoomFileForm from './RoomFileForm.vue'
+import { NTabPane, NTabs } from 'naive-ui'
+// ============= =TAB STYLE ===============
+import { type CSSProperties } from 'vue'
+
+const tabStyle = computed<CSSProperties>(() => ({
+  width: '200px',
+  minWidth: '100px',
+  minHeight: '50px',
+  textAlign: 'center',
+  justifyContent: 'center',
+  fontSize: '20px',
+  fontWeight: 600,
+  textColor: '#616161'
+}))
+</script>
+
+<style scoped></style>
