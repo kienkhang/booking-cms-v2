eaa53e7b22a4341fbd89ddd12b30138e7d59ada0
KienKhang <trankienkhang2210@gmail.com>
2023-05-03 23:10:47 +0700

    feat: add room, user dtos

---
+++ src/dtos/user.ts
@@ -0,0 +1,11 @@
+interface User {
+  avatar: string
+  fullname: string
+  email: string
+  phone: string
+  role: string
+  status: number
+  created_at: string
+  updated_at: string
+}
+export { User }
