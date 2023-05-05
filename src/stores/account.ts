d57e823124577e8d9c7f2a99089f34409b5a710a
KienKhang <trankienkhang2210@gmail.com>
2023-05-05 07:31:14 +0700

    feat: computed role user

--- src/stores/account.ts
+++ src/stores/account.ts
@@ -3,12 +3,23 @@ import { User } from '@/dtos/user'
 const accountStore = () => {
   const account = ref<User>()
 
+  const role = computed(() => {
+    if (+account.value.role === 1) return 'CUSTOMER'
+    else if (+account.value.role === 2) return 'HOTELIER'
+    else if (+account.value.role === 3) return 'ACCOUNTANT'
+    else if (+account.value.role === 4) return 'STAFF'
+    else if (+account.value.role === 5) return 'MANAGER'
+    else if (+account.value.role === 51) return 'ADMIN'
+    else if (+account.value.role === 66) return 'SUPPERADMIN'
+  })
+
   const setAccount = (acc: User) => {
     account.value = acc
   }
 
   return {
     account,
+    role,
     setAccount
   }
 }
