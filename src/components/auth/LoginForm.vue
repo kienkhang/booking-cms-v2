9e690283252303e82e7520ffa90167871502f295
KienKhang <trankienkhang2210@gmail.com>
2023-05-05 16:38:30 +0700

    feat: update length to testing

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -4,7 +4,7 @@
 
   FormKit(type='form' v-model:model-value="loginData" name='login_form' id='login_form' @submit="doLogin()" :actions='false' style='width: 100%; padding: 16px;')
     FormKit(type='text' label='Email' name="email" placeholder='Nhập email của bạn' validation="required|email")
-    FormKit(type="password" label='Password' name='password' placeholder='Nhập mật khẩu của bạn' validation="required|length:6")
+    FormKit(type="password" label='Password' name='password' placeholder='Nhập mật khẩu của bạn' validation="required|length:1")
     FormKit.block.w-full(type="submit" name='login' input-class='bg-green-500') Login
     FormKit.block.w-full(type="button" input-class='bg-red-500' @click='doRefresh()') Refresh
     FormKit.block.w-full(type="button" input-class='bg-purple-500' @click='doGetMe()') Details
