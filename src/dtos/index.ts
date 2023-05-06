886ad4c5ab1cd5ace71a9e3d9b65443b73f37984
KienKhang <trankienkhang2210@gmail.com>
2023-05-07 02:48:09 +0700

    feat: update dtos index

---
+++ src/dtos/index.ts
@@ -0,0 +1,18 @@
+import { IHotel, IHotelAdd, IHotelUpdate } from './hotel'
+import { IRoom } from './room'
+import { IUser, IAddUser, IUpdateUser } from './user'
+import { IFormKitFile, IDateCell, IRoomNightCell, IRatePackageCell } from './shared'
+
+export {
+  IHotel,
+  IHotelAdd,
+  IHotelUpdate,
+  IRoom,
+  IUser,
+  IAddUser,
+  IUpdateUser,
+  IFormKitFile,
+  IDateCell,
+  IRoomNightCell,
+  IRatePackageCell
+}
