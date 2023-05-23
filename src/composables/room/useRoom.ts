001f668aeb8dc0eba431e2c33818dc0bee6e26c3
kienkhang <trankienkhang2210@gmail.com>
2023-05-23 18:06:19 +0700

    feat: useRoom

--- src/composables/room/useRoom.ts
+++ src/composables/room/useRoom.ts
@@ -1,4 +1,70 @@
+import { roomsApi } from '@/apis/room'
+import type { IRoom } from '@/dtos'
+
 function useRoom() {
-  return {}
+  // Import naive ui message
+  const message = useMessage()
+  // Import state from store
+  const { rooms, currentRoom, roomCount, filter, paging } = storeToRefs(useRoomStore())
+  // Import actions from store
+  const { getRooms } = useRoomStore()
+
+  // Create room
+  function createRoom(form: Omit<IRoom, 'id' | 'photos'>) {
+    const usedCreateRoom = roomsApi.createRoom(form)
+    const { isFinished, error, execute } = usedCreateRoom
+
+    // until isFinished => getRoom and show message
+    until(isFinished)
+      .toBeTruthy()
+      .then(() => {
+        if (!error.value) {
+          getRooms()
+          message.success('Tạo khách sạn thành công')
+          return
+        }
+        message.success('Tạo khách sạn thất bại')
+      })
+    // Return all field useAxios
+    return {
+      ...usedCreateRoom,
+      executeApi: () => {
+        execute({ data: { ...form } })
+      }
+    }
+  }
+  // Update room
+  function updateRoom(form: Omit<IRoom, 'id' | 'photos'>, roomId: string) {
+    const usedUpdateRoom = roomsApi.updateRoom(form, roomId)
+    const { isFinished, error, execute } = usedUpdateRoom
+
+    // until isFinished => show message
+    until(isFinished)
+      .toBeTruthy()
+      .then(() => {
+        if (!error.value) {
+          message.success('Cập nhật khách sạn thành công')
+          return
+        }
+        message.success('Cập nhật khách sạn thất bại')
+      })
+
+    return {
+      ...usedUpdateRoom,
+      executeApi: () => {
+        execute({ data: { ...form } })
+      }
+    }
+  }
+
+  return {
+    rooms,
+    currentRoom,
+    roomCount,
+    paging,
+    filter,
+    createRoom,
+    updateRoom
+  }
 }
 export default useRoom
