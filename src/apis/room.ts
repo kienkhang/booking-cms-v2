cb4c40be925420b1b79be165fd849c0dfee4025b
kienkhang <trankienkhang2210@gmail.com>
2023-06-01 17:33:05 +0700

    feat: change path call api

--- src/apis/room.ts
+++ src/apis/room.ts
@@ -1,4 +1,4 @@
-const path = 'rooms/'
+const path = 'rooms'
 // const admin = 'admin/'
 class Room {
   createRoom = (data: any | {}) => {
@@ -14,19 +14,19 @@ class Room {
     return useGet({ url, requiredToken: true })
   }
   upsertRoomNight = (data: any | {}) => {
-    const url = path + 'roomnights'
+    const url = path + '/roomnights'
     return usePost({ url, data, requiredToken: true })
   }
   upsertRatePakage = (data: any | {}) => {
-    const url = path + 'ratepackages'
+    const url = path + '/ratepackages'
     return usePost({ url, data, requiredToken: true })
   }
   getInventories = (params: any | {}, roomId: string) => {
-    const url = path + `${roomId}/inventories`
+    const url = path + `/${roomId}/inventories`
     return useGet({ url, params, requiredToken: true })
   }
   getRatePlans = (params: any | {}, roomId: string) => {
-    const url = path + `${roomId}/rateplans`
+    const url = path + `/${roomId}/rateplans`
     return useGet({ url, params, requiredToken: true })
   }
 }
