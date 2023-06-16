77cc5bb423b6122465c7d95deea215abedb31196
kienkhang <trankienkhang2210@gmail.com>
2023-06-16 11:57:48 +0700

    feat: navigation guard

--- src/modules/pinia.ts
+++ src/modules/pinia.ts
@@ -3,39 +3,63 @@ import type { Router } from 'vue-router'
 import type { UserModule } from '@/types'
 import accountStore from '@/stores/account'
 
-const navigationAuthGuard = (router: Router) => {
-  const { getMe } = useAuth()
+function navigationAuthGuard(router: Router) {
+  // Destruct account from store
   const { account } = storeToRefs(accountStore())
+  // Destruct get Hotel from local function
+  // const { getHotelLocalStore } = useHotelsStore()
+  // Destruct get profile from composables
+  const { getMe } = useAuth()
+  // Destruct isLoggedIn status from cookie
   const { isLoggedIn } = useAuthStorage()
-  const { getHotelLocalStore } = useHotelsStore()
-  const { hotelId } = useHotelStorage()
-  router.beforeEach(async (to, _, next) => {
+  // Destruct hotelId from localstorage
+  // const { hotelId } = useHotelStorage()
+  router.beforeEach(async (to, from, next) => {
+    // Check location to authen
     const requiredAuth = to.meta.requiredAuth
-    const requiredHotel = to.meta.requiredHotel
-    // if reload or redirect to any page (logged in) but not account -> get me
-    if (isLoggedIn() && !account.value) {
-      await getMe().executeAPI()
-      // if exist hotel in local and not having current Hotel -> get hotel
-      if (hotelId.value && requiredHotel) {
-        getHotelLocalStore()
-      }
-      // from catch navigate to "/" and to catch case reload
-      // if need hotel and not hotel saved in store -> goto select
-      if (requiredHotel && !hotelId.value) {
-        next({ name: 'select' })
-      }
-    } else if (account.value && hotelId.value && requiredHotel) {
-      getHotelLocalStore()
-    } else if (requiredHotel && !hotelId.value) {
-      next({ name: 'select' })
-    }
+    // const requiredHotel = to.meta.requiredHotel
+
     // from catch navigate to "/" and to catch case reload
     // if need authen and not login -> goto login
     if (requiredAuth && !isLoggedIn()) {
-      next({ name: 'auth' })
+      return next({ name: 'auth' })
     }
-    // else next
-    else next()
+
+    // if reload or redirect to any page (logged in) but not account -> get me
+    if (isLoggedIn() && !account.value && requiredAuth) {
+      await getMe().executeAPI()
+      // hotelGuard(to, next)
+    }
+    // if (account.value) {
+    //   hotelGuard(to, from)
+    // }
+
+    return next()
+  })
+}
+
+function navigationHotelGuard(router: Router) {
+  // Destruct account from store
+  const { account } = storeToRefs(accountStore())
+  // Destruct get Hotel from local function
+  const { getHotelLocalStore } = useHotelsStore()
+  // Destruct hotelId from localstorage
+  const { hotelId } = useHotelStorage()
+  router.beforeEach((to, from, next) => {
+    // Check location to required hotel
+    const requiredHotel = to.meta.requiredHotel
+    // if exist hotel in local and not having current Hotel -> get hotel
+    if (hotelId.value && requiredHotel && account.value) {
+      getHotelLocalStore()
+    }
+    // from catch navigate to "/" and to catch case reload
+    // if need hotel and not hotel saved in store -> goto select
+    else if (!hotelId.value && requiredHotel) {
+      if (from.name !== 'select') {
+        return next({ name: 'select' })
+      }
+    }
+    return next()
   })
 }
 
@@ -52,4 +76,5 @@ export const install: UserModule = ({ isClient, initialState, app, router }) => 
 
   // Navigation guard
   navigationAuthGuard(router)
+  navigationHotelGuard(router)
 }
