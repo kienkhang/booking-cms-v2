986acffe1b744828b3def06ed35429b27668e8e7
KienKhang <trankienkhang2210@gmail.com>
2023-06-18 16:42:47 +0700

    feat: remove unnecessary field and button

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -3,17 +3,17 @@
   //- .text-3xl.font-bold Đăng nhập
 
   FormKit(type='form' v-model:model-value="loginData" name='login_form' id='login_form' @submit="doLogin()" :actions='false' style='width: 100%; padding: 16px;')
-    FormKit(type='text' label='Email' name="email" placeholder='Nhập email của bạn' validation="required|email")
-    FormKit(type="password" label='Password' name='password' placeholder='Nhập mật khẩu của bạn' validation="required|length:1")
-    FormKit.block.w-full(type="submit" name='login' input-class='bg-green-500') Login
-    FormKit.block.w-full(type="button" input-class='bg-red-500' @click='doRefresh()') Refresh
-    FormKit.block.w-full(type="button" input-class='bg-purple-500' @click='doGetMe()') Details
+    .flex.flex-col.gap-2
+      FormKit(type='text' label='Email' name="email" placeholder='Nhập email của bạn' validation="required|email")
+      FormKit(type="password" label='Password' name='password' placeholder='Nhập mật khẩu của bạn' validation="required|length:1")
+      FormKit.block.w-full(type="submit" name='login' input-class='w-full bg-crayola') Login
     
 
 </template>
 
 <script setup lang="ts">
 import useAuth from '@/composables/useAuth'
+import type { AxiosError } from 'axios'
 
 const router = useRouter()
 
@@ -22,22 +22,21 @@ const loginData = ref({
   email: '',
   password: ''
 })
-const { getRToken } = useAuthStorage()
-const refreshToken = getRToken()
-const { login, refresh, getMe } = useAuth()
+//
+const mess = useMessage()
+
+const { login } = useAuth()
 
 const { executeAPI: exeLogin } = login(loginData.value)
-const { executeAPI: exeRefresh } = refresh({ refresh_token: refreshToken })
-const { executeAPI: exeGetMe } = getMe()
 
 const doLogin = () =>
-  exeLogin().then((resp) => {
-    if (resp.data.value) {
+  exeLogin()
+    .then(() => {
       router.push('/')
-    }
-  })
-const doRefresh = () => exeRefresh()
-const doGetMe = () => exeGetMe()
+    })
+    .catch(function (e: AxiosError) {
+      mess.error((e.response.data as any).message)
+    })
 </script>
 
 <style scoped></style>
